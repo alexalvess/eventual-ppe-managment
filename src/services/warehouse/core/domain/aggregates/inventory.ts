0f6eef9357b3ce4aa3330451af4014ee4d784f62
Alex Alves <alexalves2501@hotmail.com>
2023-03-24 17:08:05 -0300

    include behaviors in aggregate

--- src/services/warehouse/core/domain/aggregates/inventory.ts
+++ src/services/warehouse/core/domain/aggregates/inventory.ts
@@ -2,11 +2,15 @@ import { IDomainEvent } from "../../../../../contracts/abstractions/ievent"
 import { CreateInventory } from "../../../../../contracts/services/warehouse/command";
 import AggregateRoot from "../abstractions/aggregates/aggregateRoot"
 import Item from "../entities/item";
+import { v4 } from "uuid";
 
 export default class Inventory extends AggregateRoot {
     availableQuantity: number = 0;
 
     private readonly items: Array<Item> = new Array<Item>();
+
+    public createInventory(command: CreateInventory): void {
+    }
 
     addAvailableAmount(amount: number): void {
         this.availableQuantity = amount;
