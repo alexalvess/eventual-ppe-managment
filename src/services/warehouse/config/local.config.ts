a0f764de1b81c091bba0ba2fcf01371a471eea9b
Alex Alves <alexalves2501@hotmail.com>
2023-04-12 16:54:16 -0300

    include environment variables

--- src/services/warehouse/config/local.config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-    "connectionStrings": {
-        "Projections": "mongodb://mongoadmin:secret@127.0.0.1:27017/ppe-managment/?authSource=admin"
-    }
-}
---
+++ src/services/warehouse/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    staging: false,
+    development: false,
+    connectionStrings: {
+        projections: "mongodb://mongoadmin:secret@127.0.0.1:27017/ppe-managment/?authSource=admin"
+    }
+};
