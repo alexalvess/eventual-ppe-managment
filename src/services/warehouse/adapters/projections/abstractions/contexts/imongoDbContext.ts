c0302b82db35e587b960f6543f25945e5ec4d3b6
Alex Alves <alexalves2501@hotmail.com>
2023-04-12 17:53:57 -0300

    move db context to abstractions

---
+++ src/services/warehouse/adapters/projections/abstractions/contexts/imongoDbContext.ts
@@ -0,0 +1,6 @@
+import { Collection } from "mongodb";
+import { IDisposable } from "../../../../../../utils/idisposable";
+
+export interface IMongoDbContext extends IDisposable {
+    getColletion<TSchema extends Document = Document>(collectionName: string): Promise<Collection<TSchema>>;
+}
