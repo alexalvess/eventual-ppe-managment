7b24ec2e15f2d6a48529f0bfb2efe58e6b59b016
Alex Alves <alexalves2501@hotmail.com>
2023-04-12 17:00:26 -0300

    fix environment config

--- src/services/warehouse/environments/environment.ts
+++ src/services/warehouse/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-    production: false,
-    staging: false,
-    development: false,
     connectionStrings: {
         projections: "mongodb://mongoadmin:secret@127.0.0.1:27017/ppe-managment/?authSource=admin"
     }
