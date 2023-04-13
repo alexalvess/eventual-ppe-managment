2ae4d22a9c73463cdea58e0955d05c9cde88d405
Alex Alves <alexalves2501@hotmail.com>
2023-04-13 10:28:37 -0300

    include find method

--- src/services/warehouse/adapters/projections/projectionGateway.ts
+++ src/services/warehouse/adapters/projections/projectionGateway.ts
@@ -1,4 +1,17 @@
+import { CancellationToken } from "typescript";
+import { IMongoDbContext } from "./abstractions/contexts/imongoDbContext";
+import * as Mongoose from "mongoose";
 
-export class ProjectionGateway<TSchema extends Document = Document> {
-    
+export class ProjectionGateway<TSchema extends Mongoose.AnyObject = Mongoose.AnyObject> {
+    private readonly context: IMongoDbContext;
+    private readonly collectionName: string;
+
+    constructor(context: IMongoDbContext, collectionName: string) {
+        this.context = context;
+        this.collectionName = collectionName;
+    }
+
+    public findAsync(filter: Mongoose.mongo.Filter<TSchema>, cancellationToken: CancellationToken): Promise<Mongoose.mongo.WithId<TSchema> | null> {
+        return this.context.getColletion<TSchema>(this.collectionName).findOne(filter);
+    }
 }
