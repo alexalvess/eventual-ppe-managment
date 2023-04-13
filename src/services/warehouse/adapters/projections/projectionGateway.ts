da248081c8448652ae71f7c9821374c605279180
Alex Alves <alexalves2501@hotmail.com>
2023-04-13 11:12:11 -0300

    remove cancellation token

--- src/services/warehouse/adapters/projections/projectionGateway.ts
+++ src/services/warehouse/adapters/projections/projectionGateway.ts
@@ -5,22 +5,17 @@ import * as Mongoose from "mongoose";
 export class ProjectionGateway<TSchema extends Mongoose.AnyObject = Mongoose.AnyObject> {
     private readonly context: IMongoDbContext;
     private readonly collectionName: string;
-
+ 
     constructor(context: IMongoDbContext, collectionName: string) {
         this.context = context;
         this.collectionName = collectionName;
     }
-
-    public findAsync(filter: {}, cancellationToken: CancellationToken): Promise<TSchema | null> {
-        return this.context.getColletion<TSchema>(this.collectionName)
-            .findOne<TSchema>(filter)
-            .then(item => {
-                cancellationToken.throwIfCancellationRequested();
-                return item;
-            });
+ 
+    public findAsync(filter: {}): Promise<TSchema | null> {
+        return this.context.getColletion<TSchema>(this.collectionName).findOne<TSchema>(filter);
     }
-
-    public getAsync(id: string, cancellationToken: CancellationToken): Promise<TSchema | null> {
-        return this.findAsync({ _id: id }, cancellationToken);
+ 
+    public getAsync(id: string): Promise<TSchema | null> {
+        return this.findAsync({ _id: id });
     }
 }
