4bc862d0ef44ca0abcb7ef236d607041457ec8ad
Alex Alves <alexalves2501@hotmail.com>
2023-04-14 16:18:46 -0300

    configure broker bindings

--- src/services/warehouse/environments/environment.ts
+++ src/services/warehouse/environments/environment.ts
@@ -70,12 +70,42 @@ export const environment = {
 
                  ],
                  bindings: {
-                    "b1": {
-                       "source": "test_exchange",
-                       "destination": "test_queue",
-                       "destinationType": "queue",
-                       "bindingKey": "test_route"
-                    }
+                    create_inventory_binding: {
+                       source: "create_inventory_exchange",
+                       destination: "inventory.create_inventory",
+                       destinationType: "queue",
+                       bindingKey: "create_inventory_route"
+                    },
+                    increase_inventory_binding: {
+                        source: "increase_inventory_exchange",
+                        destination: "inventory.increase_inventory",
+                        destinationType: "queue",
+                        bindingKey: "increase_inventory_route"
+                    },
+                    decrease_inventory_binding: {
+                        source: "decrease_inventory_exchange",
+                        destination: "inventory.decrease_inventory",
+                        destinationType: "queue",
+                        bindingKey: "decrease_inventory_route"
+                    },
+                    inventory_created_binding: {
+                        source: "inventory_created_exchange",
+                        destination: "inventory.inventory_created",
+                        destinationType: "queue",
+                        bindingKey: "inventory_created_route"
+                     },
+                     inventory_increased_binding: {
+                        source: "inventory_increased_exchange",
+                        destination: "inventory.inventory_increased",
+                        destinationType: "queue",
+                        bindingKey: "inventory_increased_route"
+                     },
+                     inventory_decreased_binding: {
+                        source: "inventory_decreased_exchange",
+                        destination: "inventory.inventory_decreased",
+                        destinationType: "queue",
+                        bindingKey: "inventory_decreased_route"
+                     },
                  },
                  "publications": {
                     "demo_publication": {
