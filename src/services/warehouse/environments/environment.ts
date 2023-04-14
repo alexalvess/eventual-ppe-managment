5cb99b8c842ad35e7bd7ccdf54d5b02ee19b8ab7
Alex Alves <alexalves2501@hotmail.com>
2023-04-14 14:07:12 -0300

    include connection string on environment file

--- src/services/warehouse/environments/environment.ts
+++ src/services/warehouse/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     connectionStrings: {
-        projections: "mongodb://mongoadmin:secret@127.0.0.1:27017/ppe-managment/?authSource=admin"
+        projections: "mongodb://mongoadmin:secret@127.0.0.1:27017/ppe-managment/?authSource=admin",
+        broker: "amqp://localhost:5672"
     }
 };
